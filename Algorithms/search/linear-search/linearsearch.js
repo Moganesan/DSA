269a9447502a278a3bd8659f18178e39ae73c6d4
Moganesan M <ht.moganesan@gmail.com>
2021-09-30 14:50:15 +0530

    linear-search folder added

---
+++ Algorithms/search/linear-search/linearsearch.js
@@ -0,0 +1,50 @@
+let districts = [
+  "Ariyalur",
+  "Chengalpattu",
+  "Chennai",
+  "Coimbatore",
+  "Cuddalore",
+  "Dharmapuri",
+  "Dindigul",
+  "Erode",
+  "Kallakurichi",
+  "Kanchipuram",
+  "Kanyakumari",
+  "Karur",
+  "Krishnagiri",
+  "Madurai",
+  "Mayiladuthurai",
+  "Nagapattinam",
+  "Namakkal",
+  "Nilgiris",
+  "Perambalur",
+  "Pudukkottai",
+  "Ramanathapuram",
+  "Ranipet",
+  "Salem",
+  "Sivagangai",
+  "Tenkasi",
+  "Thanjavur",
+  "Theni",
+  "Thoothukudi",
+  "Tiruchirappalli",
+  "Tirunelveli",
+  "Tirupattur",
+  "Tiruppur",
+  "Tiruvallur",
+  "Tiruvannamalai",
+  "Tiruvarur",
+  "Vellore",
+  "Viluppuram",
+  "Virudhunagar",
+];
+
+function getMyCity(pattern) {
+  districts.forEach((city, index) => {
+    if (pattern === city) {
+      console.log(city, index);
+    }
+  });
+}
+
+getMyCity("Virudhunagar");
