4cee87ef1ceb3ff78b69a50bfbcb9b2965702cbe
Moganesan M <ht.moganesan@gmail.com>
2021-10-02 23:36:10 +0530

    comit

--- Algorithms/search/binary-search/binarysearch.js
+++ Algorithms/search/binary-search/binarysearch.js
@@ -4,9 +4,8 @@ function binarySearch(array, number) {
   let mid = 0;
   while (start <= end) {
     mid = Math.floor((start + end) / 2);
-    console.log(array);
     if (array[mid] == number) {
-      return array[mid];
+      return "Found";
     } else if (number > array[mid]) {
       console.log(array.slice(mid + 1, end + 1));
       start = mid + 1;
@@ -19,8 +18,10 @@ function binarySearch(array, number) {
   return -1;
 }
 
-let array = [1, 3, 5, 7, 8, 9];
+let array = [1, 3, 5, 7, 8, 9, 273, 2323, 34, 22, 44, 11, 66];
 
-let sortedArray = array.sort();
+let sortedArray = array.sort(function (a, b) {
+  return a - b;
+});
 
-console.log(binarySearch(array, 3));
+console.log(binarySearch(sortedArray, 11));
