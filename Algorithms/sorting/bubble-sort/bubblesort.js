883efa10187e898c04e0cf446d7ca1692f363185
Moganesan M <ht.moganesan@gmail.com>
2021-10-03 11:57:24 +0530

    bubble sort

--- Algorithms/sorting/bubble-sort/bubblesort.js
+++ Algorithms/sorting/bubble-sort/bubblesort.js
@@ -1,14 +1,31 @@
-let array = [1, 2, 5, 3, 8, 12, 11, 19, 33, 30, 80, 20, 29];
+let array = [9, 1, 2, 3, 4, 5, 6, 7, 8];
 
+// //unoptimized version
+// function bubbleSort(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length - 1; j++) {
+//       console.log(array, array[j], array[j + 1]);
+//       if (array[j] > array[j + 1]) {
+//         [array[j], array[j + 1]] = [array[j + 1], array[j]];
+//       }
+//     }
+//   }
+// }
+
+//optimized version
 function bubbleSort(array) {
+  let noSwap;
   for (let i = 0; i < array.length; i++) {
+    noSwap = true;
     for (let j = 0; j < array.length - 1; j++) {
       if (array[j] > array[j + 1]) {
         [array[j], array[j + 1]] = [array[j + 1], array[j]];
-        console.log(array);
+        noSwap = false;
       }
     }
+    if (noSwap) break;
   }
+
   console.log(array);
 }
 
